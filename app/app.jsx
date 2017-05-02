e0456a828ddd7c8e5e9462c46e7e52717e9c19f1
Andrew James <ajames20@gmail.com>
2017-05-02 15:50:46 +0300

    switched to sass

--- app/app.jsx
+++ app/app.jsx
@@ -13,7 +13,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation()
 
 //CSs
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
